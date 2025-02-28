f6d6647253129fa082182848fbf9d1d998083005
tonmoy-free <tonmoykhan.free@gmail.com>
2025-02-28 22:38:19 +0600

    card title section design done

---
+++ js/bgColorChange.js
@@ -0,0 +1,16 @@
+// const bg =  document.getElementById("bg-color-btn")
+// const colors = ['bg-red-500', 'bg-green-500', 'bg-yellow-500', 'bg-blue-500'];
+
+// const body = document.getElementById("body");
+// // body.classList.add("bg-red-600");
+// // for( let color of colors){
+// //     bg.addEventListener("click",function(event){
+// //         console.log(color[1]);
+// //     })
+// // }
+// for(let i=0; i<colors.length; i++){
+//     bg.addEventListener("click",function(event){
+//         event.target.classList.add(colors[i]);
+        
+//     })
+// }
