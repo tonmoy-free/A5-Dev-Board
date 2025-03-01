234161b803d006b1bc78e1fa518388e67e1eb58a
tonmoy-free <tonmoykhan.free@gmail.com>
2025-03-01 14:48:57 +0600

    activity log design complete

--- js/bgColorChange.js
+++ js/bgColorChange.js
@@ -8,9 +8,28 @@
 // //         console.log(color[1]);
 // //     })
 // // }
-// for(let i=0; i<colors.length; i++){
-//     bg.addEventListener("click",function(event){
-//         event.target.classList.add(colors[i]);
-        
-//     })
-// }
+
+
+const completeButtons = document.querySelectorAll('.complete-button');
+
+    for (let i = 0; i < completeButtons.length; i++) {
+      completeButtons[i].addEventListener('click', function(event) {
+        const numberCount = document.getElementById("number");
+        let number = parseInt(numberCount.innerText);
+        number -= 1;
+        numberCount.innerText = number;
+
+        const totalNumber =document.getElementById('total-number')
+        let totalNumberInt = parseInt(totalNumber.innerText);
+        totalNumberInt +=1;
+         totalNumber.innerText = totalNumberInt;
+
+
+        event.target.classList.add('disabled');
+        event.target.disabled = true;
+      });
+    }
+
+
+
+
